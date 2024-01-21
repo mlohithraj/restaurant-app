3a162114a04a018429fa5b3711f0fd10e3b4d697
mlohithraj <rlohith767@gmail.com>
2024-01-21 18:49:37 +0530

    Adding summary to the app

--- src/components/Layout/Header.js
+++ src/components/Layout/Header.js
@@ -13,6 +13,13 @@ const Header = () => {
       <div className={classes['main-image']}>
         <img src={mealsImage} alt='A table full of delicious food!' />
       </div>
+      <div className={classes.summary}>
+        <h1>Delicious Food, Delivered to you</h1>
+        <div className={classes.paragraph}>
+        <p>Choose your favourite meal from our broad section of available meals and enjoy a delicious lunch or dinner at home</p>
+        <p>All our meals are cooked with high quality ingredients, just in time and of course by <br/> experienced chefs!</p>
+        </div>
+      </div>
     </Fragment>
   )
 }
