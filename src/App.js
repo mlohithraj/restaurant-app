e73d73f2f0c7c84b602f082a3d221d565b5769c0
mlohithraj <rlohith767@gmail.com>
2024-01-23 06:50:52 +0530

    Added a cart

--- src/App.js
+++ src/App.js
@@ -3,10 +3,12 @@ import { Fragment } from 'react';
 import './App.css';
 import Header from './components/Layout/Header';
 import Meals from './components/Meals/Meals';
+import Cart from './components/Cart/Cart';
 
 function App() {
   return (
     <Fragment className="App">
+          <Cart/>
         <Header/>
         <Meals/>
     </Fragment>
---
+++ src/components/Cart/Cart.js
@@ -0,0 +1,33 @@
+import React from 'react'
+import classes from './Cart.module.css'
+import Modal from '../UI/Modal'
+
+const Cart = (props) => {
+
+    const cartItems = <ul className={classes['cart-items']}>{[{
+        id: 'm1',
+        name: 'roll',
+        amount: '2',
+        price: '70'
+    }].map((items) =>
+    <li>
+        {items.name}
+    </li>)}
+    </ul>
+
+  return (
+    <Modal>
+        {cartItems}
+        <div className={classes.total}> 
+            <span>Total Amount</span>
+            <span>40</span>
+        </div>
+        <div className={classes.actions}>
+            <button className={classes['button-alt']}>Close</button>
+            <button className={classes.button}>Order</button>
+        </div>
+    </Modal>
+  )
+}
+
+export default Cart
